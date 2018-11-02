7c01e475970eb518ebb66050946591127817a3fe
kvdh2000 <kishanvdh@gmail.com>
2018-11-02 13:53:03 +0100

    Update Game/wwwroot/Functions/Models.js

--- Game/wwwroot/Functions/Models.js
+++ Game/wwwroot/Functions/Models.js
@@ -2,257 +2,265 @@
 //returns array
 function shuffle(array)
 {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex)
-  {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex)
+	{
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-  return array;
+	return array;
 }
 
 //creates an array of all models for the game to pull from
 function createModelPool()
 {
-  var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  arr = shuffle(arr);
-  var newModel;
-  for (var i = 0; i < arr.length; i++)
-  {
-    newModel = createModel(arr[i]);
-    modelPool.push(newModel);
-  }
+	var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	arr = shuffle(arr);
+	var newModel;
+	for (var i = 0; i < arr.length; i++)
+	{
+		newModel = createModel(arr[i]);
+		modelPool.push(newModel);
+	}
 }
 
 //returns model given an index from 0/9
 function createModel(index)
 {
-  console.log(index);
+	console.log(index);
 
-  switch (index)
-  {
-    case 0:
-      {
-        model = new gAllosaurus;
-        break;
-      }
+	switch (index)
+	{
+		case 0:
+			{
+				model = new gAllosaurus;
+				break;
+			}
 
-    case 1:
-      {
-        model = new gAnkylosaurus;
-        break;
-      }
+		case 1:
+			{
+				model = new gAnkylosaurus;
+				break;
+			}
 
-    case 2:
-      {
-        model = new gBrachiosaurus;
-        break;
-      }
+		case 2:
+			{
+				model = new gBrachiosaurus;
+				break;
+			}
 
-    case 3:
-      {
-        model = new gDilophosaurus;
-        break;
-      }
+		case 3:
+			{
+				model = new gDilophosaurus;
+				break;
+			}
 
-    case 4:
-      {
-        model = new gGallimimus;
-        break;
-      }
+		case 4:
+			{
+				model = new gGallimimus;
+				break;
+			}
 
-    case 5:
-      {
-        model = new gParasaurolophus;
-        break;
-      }
+		case 5:
+			{
+				model = new gParasaurolophus;
+				break;
+			}
 
-    case 6:
-      {
-        model = new gSpinosaurus;
-        break;
-      }
+		case 6:
+			{
+				model = new gSpinosaurus;
+				break;
+			}
 
-    case 7:
-      {
-        model = new gStegosaurus;
-        break;
-      }
+		case 7:
+			{
+				model = new gStegosaurus;
+				break;
+			}
 
-    case 8:
-      {
-        model = new gTRex;
-        break;
-      }
+		case 8:
+			{
+				model = new gTRex;
+				break;
+			}
 
-    case 9:
-      {
-        model = new gTriceratops;
-        break;
-      }
+		case 9:
+			{
+				model = new gTriceratops;
+				break;
+			}
 
-    default:
-      {
-        break;
-      }
-  }
-  return model;
+		default:
+			{
+				break;
+			}
+	}
+	return model;
 }
 
 //determines which lanes will have an obstacle
 function addPath()
 {
-  var lanes = [0, 1, 2];
-  var lane = Math.floor(Math.random() * 3);
-  addModel(true, lane);
-  lanes.splice(lane, 1);
+	var lanes = [0, 1, 2];
+	var lane = Math.floor(Math.random() * 3);
+	addModel(true, lane);
+	lanes.splice(lane, 1);
 }
 
 //adds a model to the planet using a sphericalhelper to find the edge of the sphere
 function addModel(inPath, row, isLeft)
 {
-  var newModel;
+	var newModel;
 
-  if (inPath)
-  {
-    if (modelPool.length == 0)
-    {
-      return;
-    }
+	if (inPath)
+	{
+		if (modelPool.length == 0)
+		{
+			return;
+		}
 
-    newModel = modelPool.shift();
-    var r = Math.floor(Math.random() * 10);
-    modelPool.push(createModel(r));
-    newModel.visible = true;
-    //console.log("add model");
-    dinosInPath.push(newModel);
-    sphericalHelper.set(worldRadius - 0.3, pathAngleValues[row], -planet.rotation.x + 4);
-  }
+		newModel = modelPool.shift();
+		var r = Math.floor(Math.random() * 10);
+		modelPool.push(createModel(r));
+		newModel.visible = true;
+		//console.log("add model");
+		dinosInPath.push(newModel);
+		sphericalHelper.set(worldRadius - 0.3, pathAngleValues[row], -planet.rotation.x + 4);
+	}
 
-  else  
-  {
-    return;
-  }
+	else  
+	{
+		return;
+	}
 
-  newModel.position.setFromSpherical(sphericalHelper);
-  var rollingGroundVector = planet.position.clone().normalize();
-  var collisionVector = newModel.position.clone().normalize();
-  newModel.quaternion.setFromUnitVectors(collisionVector, rollingGroundVector);
-  //newModel.rotation.y += (Math.random() * (2 * Math.PI / 25)) + -Math.PI / 25;
-  planet.add(newModel);
+	newModel.position.setFromSpherical(sphericalHelper);
+	var rollingGroundVector = planet.position.clone().normalize();
+	var collisionVector = newModel.position.clone().normalize();
+	newModel.quaternion.setFromUnitVectors(collisionVector, rollingGroundVector);
+	//newModel.rotation.y += (Math.random() * (2 * Math.PI / 25)) + -Math.PI / 25;
+	planet.add(newModel);
 }
 
 //handles the collision and collision effects
 function modelManagement()
 {
-  var obstacle;
-  var modelPos = new THREE.Vector3();
-  var modelsToRemove = [];
-  dinosInPath.forEach(function (element, index)
-  {
-    obstacle = dinosInPath[index];
-    modelPos.setFromMatrixPosition(obstacle.matrixWorld);
-    if (modelPos.z > 6 && obstacle.visible)
-    {//gone out of our view zone
-      modelsToRemove.push(obstacle);
-    } else
-    {//check collision
-      var vectors = new Array();
-      switch (obstacle.name)
-      {
-        // case "gAllosaurus":
-        // 	console.log("gAllosaurus");
-        // 	break;
+	var obstacle;
+	var modelPos = new THREE.Vector3();
+	var modelsToRemove = [];
 
-        // case "gAnkylosaurus":
-        // 	console.log("gAnkylosaurus");
-        // 	break;
+	dinosInPath.forEach(function (element, index)
+	{
+		obstacle = dinosInPath[index];
+		modelPos.setFromMatrixPosition(obstacle.matrixWorld);
 
-        case "gBrachiosaurus":
-          for (var i = 0; i < obstacle.children.length - 1; i++)
-          {
-            vectors.push(new THREE.Vector3().setFromMatrixPosition(obstacle.getObjectByName(i.toString()).matrixWorld));
-            if (i < 12)
-            {
-              vectors.push(0.3);
-            }
-            else if (i < 14)
-            {
-              vectors.push(0.85);
-            }
-            else
-            {
-              vectors.push(1.0);
-            }
-          }
-          //console.log(vectors);
-          //console.log(player.position);
-          break;
+		if (modelPos.z > 6 && obstacle.visible)
+		{//gone out of our view zone
+			modelsToRemove.push(obstacle);
+		}
 
-        // case "gDilophosaurus":
-        // 	console.log("gDilophosaurus");
-        // 	break;
+		else
+		{//check collision
+			var vectors = new Array();
 
-        // case "gGallimimus":
-        // 	console.log("gGallimimus");
-        // 	break;
+			switch (obstacle.name)
+			{
+				// case "gAllosaurus":
+				// 	console.log("gAllosaurus");
+				// 	break;
 
-        // case "gParasaurolophus":
-        // 	console.log("gParasaurolophus");
-        // 	break;
+				// case "gAnkylosaurus":
+				// 	console.log("gAnkylosaurus");
+				// 	break;
 
-        // case "gSpinosaurus":
-        // 	console.log("gSpinosaurus");
-        // 	break;
+				case "gBrachiosaurus":
+					for (var i = 0; i < obstacle.children.length - 1; i++)
+					{
+						vectors.push(new THREE.Vector3().setFromMatrixPosition(obstacle.getObjectByName(i.toString()).matrixWorld));
+						if (i < 12)
+						{
+							vectors.push(0.3);
+						}
+						else if (i < 14)
+						{
+							vectors.push(0.85);
+						}
+						else
+						{
+							vectors.push(1.0);
+						}
+					}
+					//console.log(vectors);
+					//console.log(player.position);
+					break;
 
-        // case "gStegosaurus":
-        // 	console.log("gStegosaurus");
-        // 	break;
+				// case "gDilophosaurus":
+				// 	console.log("gDilophosaurus");
+				// 	break;
 
-        // case "gTRex":
-        // 	console.log("gTRex");
-        // 	break;
+				// case "gGallimimus":
+				// 	console.log("gGallimimus");
+				// 	break;
 
-        // case "gTriceratops":
-        // 	console.log("gTriceratops");
-        // 	break;
+				// case "gParasaurolophus":
+				// 	console.log("gParasaurolophus");
+				// 	break;
 
-        default:
-          vectors.push(modelPos);
-          vectors.push(0.6);
-          break;
-      }
-      for (var i = 0; i < vectors.length; i += 2)
-      {
-        // console.log(vectors[i]);
-        if (vectors[i].distanceTo(player.position) <= vectors[i + 1])
-        {
-          console.log("hit");
-          console.log(i / 2);
-          hasCollided = true;
-          explode();
-          gameOver();
-        }
-      }
-    }
-  });
-  var fromWhere;
-  modelsToRemove.forEach(function (element, index)
-  {
-    obstacle = modelsToRemove[index];
-    fromWhere = dinosInPath.indexOf(obstacle);
-    dinosInPath.splice(fromWhere, 1);
-    modelPool.push(obstacle);
-    obstacle.visible = false;
-    //console.log("remove model");
-  });
+				// case "gSpinosaurus":
+				// 	console.log("gSpinosaurus");
+				// 	break;
+
+				// case "gStegosaurus":
+				// 	console.log("gStegosaurus");
+				// 	break;
+
+				// case "gTRex":
+				// 	console.log("gTRex");
+				// 	break;
+
+				// case "gTriceratops":
+				// 	console.log("gTriceratops");
+				// 	break;
+
+				default:
+					vectors.push(modelPos);
+					vectors.push(0.6);
+					break;
+			}
+
+			for (var i = 0; i < vectors.length; i += 2)
+			{
+				// console.log(vectors[i]);
+				if (vectors[i].distanceTo(player.position) <= vectors[i + 1])
+				{
+					console.log("hit");
+					console.log(i / 2);
+					hasCollided = true;
+					explode();
+					gameOver();
+				}
+			}
+		}
+	});
+
+	var fromWhere;
+
+	modelsToRemove.forEach(function (element, index)
+	{
+		obstacle = modelsToRemove[index];
+		fromWhere = dinosInPath.indexOf(obstacle);
+		dinosInPath.splice(fromWhere, 1);
+		modelPool.push(obstacle);
+		obstacle.visible = false;
+		//console.log("remove model");
+	});
 }
